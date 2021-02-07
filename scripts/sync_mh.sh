a4d1f872e9786daabd66cb1ffaa2b336b3bc2090
gfw-breaker <lixiaoming0513@gmail.com>
2021-02-07 22:47:19 +0800

    同步于: 2月7日22时47分

--- scripts/sync_mh.sh
+++ scripts/sync_mh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # author: gfw-breaker
 
-channels="282 251 245 140 91 328 277 81 78 365 73 87"
+channels="19 282 251 245 140 91 328 277 81 78 365 73 87"
 
 ## create dirs
 for channel in $channels ; do
@@ -12,7 +12,7 @@ done
 for channel in $channels ; do
 	file=cat$channel.js
 	#if [[ $channel == "91" ]] || [[ $channel == "81" ]] || [[ $channel == "78" ]] || [ $channel == '73' ]; then
-	if [[ ",91,81,78,73," == *,$channel,* ]]; then
+	if [[ ",19,91,81,78,73," == *,$channel,* ]]; then
 		file=cat$channel-2021.js
 	fi 
 	url="http://www.minghui.org/mh/fenlei/$channel/$file" 
