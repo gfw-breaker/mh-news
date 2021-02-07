045032b71fe935afc3c8ffe6fe370fddd683dae1
gfw-breaker <lixiaoming0513@gmail.com>
2021-02-07 22:43:43 +0800

    同步于: 2月7日22时43分

--- scripts/sync_mh.sh
+++ scripts/sync_mh.sh
@@ -13,7 +13,7 @@ for channel in $channels ; do
 	file=cat$channel.js
 	#if [[ $channel == "91" ]] || [[ $channel == "81" ]] || [[ $channel == "78" ]] || [ $channel == '73' ]; then
 	if [[ ",91,81,78,73," == *,$channel,* ]]; then
-		file=cat$channel-2020.js
+		file=cat$channel-2021.js
 	fi 
 	url="http://www.minghui.org/mh/fenlei/$channel/$file" 
 	wget -q $url -O $channel.js
