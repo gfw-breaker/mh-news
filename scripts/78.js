f248766867b666c57739b163cf7d68391955101b
gfw-breaker <lixiaoming0513@gmail.com>
2021-04-30 09:00:43 +0800

    同步于: 4月30日9时0分

---
+++ scripts/78.js
@@ -0,0 +1,240 @@
+[
+  <head>
+    <META http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Cache-control" content="max-age=60">
+    <title></title>
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="/pub/subsite/css/subsite.css">
+    <link rel="stylesheet" type="text/css" href="/pub/cmh2015/mastersubsite.css">
+  </head>
+  <body>   <link rel="stylesheet" type="text/css" href="/pub/cmh2015/css/banner-menusubsite.css?v=20210419" />
+<div id="banner-menu">
+    <div id="topsection">
+        <div style="width:49px; display: inline-block">&#160;</div>
+        <div id="toplink">
+            <span>
+                <a href="/mh/dingyue.html" title="订阅">
+                    <img src="/pub/cmh2015/images/dingyue.png" />
+                </a>
+                <img style="height:16px;margin-bottom:4px;opacity: 0.7;" src="/pub/cmh2015/images/shuxian.png" />
+            </span>
+            <span class="dropdown">
+                <img class="dropbtn" src="/pub/cmh2015/images/xuanzeyueyan.png" />
+                <div class="dropdowncontent">
+                    <ul>
+                     	<li>
+                            <a href="//big5.minghui.org" title="正體中文" target="_blank">正體中文</a>
+                        </li>
+                        <li>
+                            <a href="//bs.minghui.org/" title="波斯尼亚语" target="_blank">Bosanski</a>
+                        </li>
+                        <li>
+                            <a href="//de.minghui.org/" title="德语" target="_blank">Deutsch</a>
+                        </li>
+                        <li>
+                            <a href="//en.minghui.org/" title="英语" target="_blank">English</a>
+                        </li>
+                        <li>
+                            <a href="//es.minghui.org/" title="西班牙语" target="_blank">Español</a>
+                        </li>
+                        <li>
+                            <a href="//fa.minghui.org/" title="波斯语" target="_blank">فارسی</a>
+                        </li>
+                        <li>
+                            <a href="//fr.minghui.org/" title="法语" target="_blank">Francais</a>
+                        </li>
+                        <li>
+                            <a href="//he.minghui.org" title="希伯来语" target="_blank">עברית</a>
+                        </li>
+                        <li>
+                            <a href="//hr.minghui.org/" title="克罗地亚语" target="_blank">Hrvatski</a>
+                        </li>
+                        <li>
+                            <a href="//id.minghui.org/" title="印尼语" target="_blank">Indonesian</a>
+                        </li>
+                        <li>
+                            <a href="//it.minghui.org/" title="意大利语" target="_blank">Italiano</a>
+                        </li>
+                        <li>
+                            <a href="//jp.minghui.org/" title="日语" target="_blank">日本語</a>
+                        </li>
+                        <li>
+                            <a href="//minghui.or.kr/" title="韩语" target="_blank">한국어</a>
+                        </li>
+                        <li>
+                            <a href="//pl.minghui.org/" title="波兰语版" target="_blank">Polski</a>
+                        </li>
+                        <li>
+                            <a href="//pt.minghui.org" title="葡萄牙语" target="_blank">Português</a>
+                        </li>
+                        <li>
+                            <a href="//ru.minghui.org/" title="俄语" target="_blank">Русский</a>
+                        </li>
+                        <li>
+                            <a href="//sk.minghui.org/" title="斯洛伐克语" target="_blank">Slovenčina</a>
+                        </li>
+                        <li>
+                            <a href="//sr.minghui.org/" title="塞尔维亚语" target="_blank">Српски</a>
+                        </li>
+                        <li>
+                            <a href="//vn.minghui.org" title="越南语" target="_blank">tiếng Việt</a>
+                        </li>
+                       
+                    </ul>
+                </div>
+            </span>
+        </div>
+    </div>
+    <div class="banner_links">
+        <a class="banner1" href="/" title="法轮大法明慧网"></a>
+        <a class="banner2" href="/" title="法轮大法明慧网"></a>
+    </div>
+
+   
+</div>
+<div class="menubar_links">
+
+    <div class="menubar_top">
+        <a href="/mh/subsite/ssxw/index.html" title=""><span class="menu11"><img src="/pub/cmh2015/images/mhxinwen.png" /></span></a>
+        <span class="menubar_separator1"><img src="/pub/cmh2015/images/shuxian.png" /></span>
+        <a href="/mh/subsite/rdzt/index.html" title=""><span class="menu21"><img src="/pub/cmh2015/images/redianzhuanti.png" /></span></a>
+        <span class="menubar_separator1"><img src="/pub/cmh2015/images/shuxian.png" /></span>
+        <a href="/mh/subsite/qkw/index.html" title=""><span class="menu31"><img src="/pub/cmh2015/images/mhqikan.png" /></span></a>
+        <span class="menubar_separator1"><img  src="/pub/cmh2015/images/shuxian.png" /></span>
+        <a href="/mh/subsite/xlyd/index.html" title=""><span class="menu41"><img src="/pub/cmh2015/images/mh-xiulianyuandi.png" /></span></a>
+        <span class="menubar_separator1"><img  src="/pub/cmh2015/images/shuxian.png" /></span>
+        <a href="/mh/subsite/whpd/index.html" title=""><span class="menu51"><img src="/pub/cmh2015/images/mh-wenhuapindao.png" /></span></a>
+        <span class="menubar_separator1"><img  src="/pub/cmh2015/images/shuxian.png" /></span>
+        <a href="/mh/subsite/jng/index.html" title=""><span class="menujng"><img src="/pub/cmh2015/images/mh-jinianguan.png" /></span></a>
+        <span class="menubar_separator1"><img src="/pub/cmh2015/images/shuxian.png" /></span>
+        <a href="//search.minghui.org/cgi-bin/search.php" title="检索"><span class="menu71"><img src="/pub/cmh2015/images/jiansuo.png" /></span></a>
+    </div>
+
+</div>            <div id="searchbox">
+            <form action='//search.minghui.org/cgi-bin/search.php' method='get' accept-charset="UTF-8">
+            <input type="text" value="" maxlength="255" name="str" class="inputtext" autocomplete="off"><input type="image" src="/pub/cmh2015/images/button_search.png" title="启动检索" class="inputimage">
+            </form>
+        <!-- end searchbox --></div>
+ 
+    <div style="text-align: center;font-size: 30px;font-weight: bold;margin-top:50px;">
+        在建设中 ...
+    </div>
+      ﻿<div class="footer black">
+    <div class="footer_left">
+        <div class="footer_list1">
+            <div>
+                <div class="footercopyrighttitle">
+
+                    明慧网版权所有<span class="en"> © </span>1999-2021
+
+                </div>
+                <div class="footercopyrighttitle2">
+                    <span>
+                        本文章或节目明慧网版权所有，非盈利转载请注明来源明慧网，<br />并包含明慧网原文标题及原文链接。
+                    </span>
+                </div>
+                <div class="footercopyrighttitle2">
+                    <a href="/mh/about-us.html">关于我们</a>
+                </div>
+                <div class="footercopyrighttitle2">
+                    <span class="footerontactwomen">
+                        <a href="/mh/submit.html">投稿说明</a>
+                    </span>
+                </div>
+                <div class="footercopyrighttitle2">
+                    <span class="footerontactwomen">
+                        <a href="/mh/contact-us.html">联系我们</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer_middle">
+        <div style=" display: flex; ">
+            <div>
+                <ul class="footercol2">
+                    <li>
+                        <a href="//big5.minghui.org/" onclick="javascript:return change_language('b5');">
+                            正體中文
+                        </a>
+                    </li>
+                    <li>
+                        <a href="//www.minghui.org/" onclick="javascript:return change_language('gb');">
+                            简体中文
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul class="footercol3 footerIntl">
+                    <li>
+                        <a href="//bs.minghui.org/" title="波斯尼亚语" target="_blank">Bosanski</a>
+                    </li>
+                    <li>
+                        <a href="//de.minghui.org/" title="德语" target="_blank">Deutsch</a>
+                    </li>
+                    <li>
+                        <a href="//en.minghui.org/" title="英语" target="_blank">English</a>
+                    </li>
+                    <li>
+                        <a href="//es.minghui.org/" title="西班牙语" target="_blank">Español</a>
+                    </li>
+                    <li>
+                        <a href="//fa.minghui.org/" title="波斯语" target="_blank">فارسی</a>
+                    </li>
+                    <li>
+                        <a href="//fr.minghui.org/" title="法语" target="_blank">Francais</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul class="footercol4 footerIntl">
+                    <li class="col2 ">
+                        <a href="//he.minghui.org" title="希伯来语" target="_blank">עברית</a>
+                    </li>
+                    <li class="col2">
+                        <a href="//hr.minghui.org/" title="克罗地亚语" target="_blank">Hrvatski</a>
+                    </li>
+                    <li class="col2">
+                        <a href="//id.minghui.org/" title="印尼语" target="_blank">Indonesian</a>
+                    </li>
+                    <li class="col2">
+                        <a href="//it.minghui.org/" title="意大利语" target="_blank">Italiano</a>
+                    </li>
+                    <li class="col2">
+                        <a href="//jp.minghui.org/" title="日语" target="_blank">日本語</a>
+                    </li>
+                    <li class="col2">
+                        <a href="//minghui.or.kr/" title="韩语" target="_blank">한국어</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul class="footercol5 footerIntl">
+                    <li class="col3 ">
+                        <a href="//pl.minghui.org/" title="波兰语版" target="_blank">Polski</a>
+                    </li>
+                    <li class="col3">
+                        <a href="//pt.minghui.org" title="葡萄牙语" target="_blank">Português</a>
+                    </li>
+                    <li class="col3">
+                        <a href="//ru.minghui.org/" title="俄语" target="_blank">Русский</a>
+                    </li>
+                    <li class="col3">
+                        <a href="//sk.minghui.org/" title="斯洛伐克语" target="_blank">Slovenčina</a>
+                    </li>
+                    <li class="col3">
+                        <a href="//sr.minghui.org/" title="塞尔维亚语" target="_blank">Српски</a>
+                    </li>
+                    <li class="col3">
+                        <a href="//vn.minghui.org" title="越南语" target="_blank">tiếng Việt</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    
+</div> 
+  </body>
+</Html>
